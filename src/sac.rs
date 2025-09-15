ea0b7ed8cce14a1d0034b8ec87173aa148567111
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2025-09-15 13:38:52 +0800

    Implement PolicyNet for SAC with Adam optimizer helper

--- src/sac.rs
+++ src/sac.rs
@@ -1,4 +1,70 @@
-use burn::prelude::Backend;
+use burn::{module::Module, nn::{Linear, LinearConfig, Relu}, optim::{adaptor::OptimizerAdaptor, AdamConfig, Optimizer}, prelude::Backend};
 use gym_rs::envs::classical_control::cartpole::CartPoleObservation;
 
 use crate::shared::data_structs::DataBatch;
+
+#[derive(Debug, Module)]
+pub struct PolicyNet<B: Backend> 
+{
+    fully_connected_layer_one: Linear<B>,
+    fully_connected_layer_mean_output: Linear<B>,
+    fully_connected_layer_standard_deviation: Linear<B>,
+    relu: Relu
+}
+
+impl PolicyNet<B: Backend> {
+    pub fn new(device: &B::Device) -> Self {
+        Self { 
+            fully_connected_layer_one: LinearConfig::new(3, 128).init(device), 
+            fully_connected_layer_mean_output: LinearConfig::new(128, 1).init(device), 
+            fully_connected_layer_standard_deviation: LinearConfig::new(128, 1).init(device), 
+            relu: Relu::new()
+        }
+    }
+    
+    pub fn forward(&mut self) {}
+}
+
+/// class PolicyNet(nn.Module):
+    // def __init__(self, learning_rate):
+    //     super(PolicyNet, self).__init__()
+    //     self.fc1 = nn.Linear(3, 128)
+    //     self.fc_mu = nn.Linear(128,1)
+    //     self.fc_std  = nn.Linear(128,1)
+    // 
+    //     this is how optimizer is initialized
+    //     self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+
+    //     self.log_alpha = torch.tensor(np.log(init_alpha))
+    //     self.log_alpha.requires_grad = True
+    //     self.log_alpha_optimizer = optim.Adam([self.log_alpha], lr=lr_alpha)
+
+    // def forward(self, x):
+    //     x = F.relu(self.fc1(x))
+    //     mu = self.fc_mu(x)
+    //     std = F.softplus(self.fc_std(x))
+    //     dist = Normal(mu, std)
+    //     action = dist.rsample()
+    //     log_prob = dist.log_prob(action)
+    //     real_action = torch.tanh(action)
+    //     real_log_prob = log_prob - torch.log(1-torch.tanh(action).pow(2) + 1e-7)
+    //     return real_action, real_log_prob
+
+    // def train_net(self, q1, q2, mini_batch):
+    //     s, _, _, _, _ = mini_batch
+    //     a, log_prob = self.forward(s)
+    //     entropy = -self.log_alpha.exp() * log_prob
+
+    //     q1_val, q2_val = q1(s,a), q2(s,a)
+    //     q1_q2 = torch.cat([q1_val, q2_val], dim=1)
+    //     min_q = torch.min(q1_q2, 1, keepdim=True)[0]
+
+    //     loss = -min_q - entropy # for gradient ascent
+    //     self.optimizer.zero_grad()
+    //     loss.mean().backward()
+    //     self.optimizer.step()
+
+    //     self.log_alpha_optimizer.zero_grad()
+    //     alpha_loss = -(self.log_alpha.exp() * (log_prob + target_entropy).detach()).mean()
+    //     alpha_loss.backward()
+    //     self.log_alpha_optimizer.step()
--- src/shared/utilities.rs
+++ src/shared/utilities.rs
@@ -1,3 +1,4 @@
+use burn::{module::AutodiffModule, optim::adaptor::OptimizerAdaptor};
 use rand::{Rng, distr::weighted::WeightedIndex, rng};
 
 /// Shared function for sampling actions from a proability
@@ -8,4 +9,14 @@ pub fn sample_action(probability: &Vec<f32>) -> Result<usize, anyhow::Error> {
     let action: usize = thread_rng.sample(distributions);
 
     Ok(action)
+}
+
+/// Initialize an Adam optimizer with default config which resembles that of PyTorch
+pub fn initialize_adam_optimizer() -> OptimizerAdaptor<Adam, AutodiffModule<B>, B> {
+    AdamConfig::new()
+        .with_beta_1(0.9)
+        .with_beta_2(0.999)
+        .with_epsilon(1e-08)
+        .with_weight_decay(Some(WeightDecayConfig::new(0.0)))
+        .init()
 }
