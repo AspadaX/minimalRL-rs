77b7a9fa69834557c2018a6e8e3a7bb993d2d6d1
xinyubao <baoxinyuworks@163.com>
2025-09-23 13:24:21 +0800

    Remove commented Python SAC implementation from forward method

--- src/sac.rs
+++ src/sac.rs
@@ -26,16 +26,6 @@ where
         }
     }
     
-    // def forward(self, x):
-    //     x = F.relu(self.fc1(x))
-    //     mu = self.fc_mu(x)
-    //     std = F.softplus(self.fc_std(x))
-    //     dist = Normal(mu, std)
-    //     action = dist.rsample()
-    //     log_prob = dist.log_prob(action)
-    //     real_action = torch.tanh(action)
-    //     real_log_prob = log_prob - torch.log(1-torch.tanh(action).pow(2) + 1e-7)
-    //     return real_action, real_log_prob
     pub fn forward<const D: usize>(&mut self, x: Tensor<B, D>) -> (Tensor<B, D>, Tensor<B, D>) {
         let x = self.relu.forward(self.fully_connected_layer_one.forward(x));
         let mean = self.fully_connected_layer_mean_output.forward(x);
@@ -53,6 +43,26 @@ where
 
         (real_action, log_probability)
     }
+
+    // def train_net(self, q1, q2, mini_batch):
+    //     s, _, _, _, _ = mini_batch
+    //     a, log_prob = self.forward(s)
+    //     entropy = -self.log_alpha.exp() * log_prob
+
+    //     q1_val, q2_val = q1(s,a), q2(s,a)
+    //     q1_q2 = torch.cat([q1_val, q2_val], dim=1)
+    //     min_q = torch.min(q1_q2, 1, keepdim=True)[0]
+
+    //     loss = -min_q - entropy # for gradient ascent
+    //     self.optimizer.zero_grad()
+    //     loss.mean().backward()
+    //     self.optimizer.step()
+
+    //     self.log_alpha_optimizer.zero_grad()
+    //     alpha_loss = -(self.log_alpha.exp() * (log_prob + target_entropy).detach()).mean()
+    //     alpha_loss.backward()
+    //     self.log_alpha_optimizer.step()
+    pub fn train_net(&self, q_net_one: QNet, q_net_two: QNet, transition: DataBatch) {}
 }
 
 // class PolicyNet(nn.Module):
